d0e0eef7779ed4e4fa2971239cf82c482f1e694b
Joelisson Santana <Joelisson997@gmail.com>
2023-10-22 15:06:20 -0300

    feat: interaca da navbar ao rolar as sections

---
+++ script/modules/selectSection.js
@@ -0,0 +1,28 @@
+export default function selectSection() {
+  const item = document.querySelectorAll(".header-nav-list-items");
+  const section = document.querySelectorAll("section");
+  let currentSection = null;
+  section.forEach((s) => {
+    const sectionVerify = s.getBoundingClientRect(); //variavel para verificar onde esta a visao do usuario
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    if (
+      sectionVerify.left < centerX &&
+      sectionVerify.right > centerX &&
+      sectionVerify.top < centerY &&
+      sectionVerify.bottom > centerY
+    ) {
+      currentSection = s.id;
+    }
+  });
+
+  item.forEach((e) => {
+    if (currentSection == e.innerText) {
+      e.style.setProperty("--width", "100%");
+    } else {
+      e.style.setProperty("--width", "30%");
+    }
+  });
+}
+window.addEventListener("scroll", selectSection);
--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 import menuHamburguer from "./modules/menuHamburguer.js";
-import { copyOnClick } from "./modules/copyOnClick.js";
+import copyOnClick from "./modules/copyOnClick.js";
+import selectSection from "./modules/selectSection.js";
 menuHamburguer()
 copyOnClick()
+selectSection()
+
