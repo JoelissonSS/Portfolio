bcc1f3ab4cd1fa6a1ddcaddf27c8f47bcc1bea72
Joelisson Santana <Joelisson997@gmail.com>
2023-10-26 12:25:38 -0300

    feat: sections habilidades>hardskills

---
+++ script/modules/scrollAutomatic.js
@@ -0,0 +1,30 @@
+export default function scrollAutomatic() {
+  const container = document.querySelector(".container-hard-skills");
+  let scroll = 2
+  function scrollContent() {
+    //scrollar ate o final
+
+    container.scroll({
+      left: container.scrollLeft+scroll,
+      behavior: "smooth"
+    })
+    //verificar se o scroll chegou no final
+    if(container.scrollLeft == container.scrollWidth  - container.offsetWidth){
+      container.scrollTo({
+        left: 0,
+        behavior: "instant",
+      })
+    }
+    // parar/continuar o scroll automatico ao passar mouse sobre o elemento
+    container.addEventListener('mousedown', () => {
+      scroll = 0
+    })
+    container.addEventListener('mouseout', () => {
+      scroll = 2
+    })
+  }
+  let scrollInterval = 100; // Intervalo de rolagem em milissegundos
+
+ setInterval(scrollContent, scrollInterval);
+  
+}
--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 import copyOnClick from "./modules/copyOnClick.js";
 import menuHamburguer from "./modules/menuHamburguer.js";
 import selectSection from "./modules/selectSection.js";
+import scrollAutomatic from "./modules/scrollautomatic.js";
 menuHamburguer();
 
 document.addEventListener("DOMContentLoaded", () => {
   copyOnClick();
   selectSection();
+  scrollAutomatic();
 });
