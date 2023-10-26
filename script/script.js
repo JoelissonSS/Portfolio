eb23c0e94bf8ee664a96b2be5ac7c3c82f95c193
Joelisson Santana <Joelisson997@gmail.com>
2023-10-26 15:09:05 -0300

    fix: script noa funcionar

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@ import menuHamburguer from "./modules/menuHamburguer.js";
 import selectSection from "./modules/selectSection.js";
 import scrollAutomatic from "./modules/scrollautomatic.js";
 menuHamburguer();
-
+scrollAutomatic();
 document.addEventListener("DOMContentLoaded", () => {
   copyOnClick();
   selectSection();
-  scrollAutomatic();
+  
 });
