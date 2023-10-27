93627cc343340fb0e4a0aedc1399d6f424632cb3
Joelisson Santana <Joelisson997@gmail.com>
2023-10-27 17:36:00 -0300

    descontinuando scroll automatico

--- script/script.js
+++ script/script.js
@@ -1,11 +1,8 @@
 import copyOnClick from "./modules/copyOnClick.js";
 import menuHamburguer from "./modules/menuHamburguer.js";
 import selectSection from "./modules/selectSection.js";
-import scrollAutomatic from "./modules/scrollAutomatic.js";
 menuHamburguer();
-scrollAutomatic();
 document.addEventListener("DOMContentLoaded", () => {
   copyOnClick();
   selectSection();
-  
 });
