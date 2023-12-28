4b2bb23545456aa3e69ac4bae6b145403a567d12
Joelisson Santana <Joelisson997@gmail.com>
2023-12-28 01:03:47 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+const init = document.querySelector('#app').style = 'display: block;'
 import copyOnClick from "./modules/copyOnClick.js";
 import menuHamburguer from "./modules/menuHamburguer.js";
 import selectSection from "./modules/selectSection.js";
@@ -9,4 +10,3 @@ const slide = new SlideNav(".slide", ".wrapper");
 slide.init();
 slide.addControl();
 
-const init = document.querySelector('#app').style = 'display: block;'
